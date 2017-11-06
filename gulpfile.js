66a15a5cdc09504d3fff851d4608feb8418bcbf2
Alexander Rechsteiner <alex@wildmind.ch>
2017-11-06 15:42:20 +0100

    Initial checkin

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp  = require('gulp'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  cleanCss = require('gulp-clean-css'),
+  scsslint = require('gulp-scss-lint'),
+  rename = require('gulp-rename'),
+  postcss      = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer');
+
+gulp.task('build-theme', function() {
+  return gulp.src(['scss/*.scss'])
+
+.pipe(scsslint({'config':'scsslint.yml'}))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([ autoprefixer({ browsers: [
+      'Chrome >= 35',
+      'Firefox >= 38',
+      'Edge >= 12',
+      'Explorer >= 10',
+      'iOS >= 8',
+      'Safari >= 8',
+      'Android 2.3',
+      'Android >= 4',
+      'Opera >= 12']})]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css/'))
+    .pipe(cleanCss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css/'))
+});
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch(['scss/*.scss'], ['build-theme']);
+});
+
+gulp.task('default', ['build-theme'], function() {
+});
+
+
+
