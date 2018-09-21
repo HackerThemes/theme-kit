5d27157f1eea8e8f7b6ffa9880208cb76a649268
AJay <ajr1396@gmail.com>
2018-09-21 15:00:08 +0530

    Added Browserify Task for Live Reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,56 @@
-var gulp  = require('gulp'),
-  sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  cleanCss = require('gulp-clean-css'),
-  rename = require('gulp-rename'),
-  postcss      = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps"),
+  cleanCss = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  browserSync = require("browser-sync").create();
 
-gulp.task('build-theme', function() {
-  return gulp.src(['scss/*.scss'])
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task("build-theme", function() {
+  return gulp
+    .src(["scss/*.scss"])
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([ autoprefixer({ browsers: [
-      'Chrome >= 35',
-      'Firefox >= 38',
-      'Edge >= 12',
-      'Explorer >= 10',
-      'iOS >= 8',
-      'Safari >= 8',
-      'Android 2.3',
-      'Android >= 4',
-      'Opera >= 12']})]))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: [
+            "Chrome >= 35",
+            "Firefox >= 38",
+            "Edge >= 12",
+            "Explorer >= 10",
+            "iOS >= 8",
+            "Safari >= 8",
+            "Android 2.3",
+            "Android >= 4",
+            "Opera >= 12"
+          ]
+        })
+      ])
+    )
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest("css/"))
     .pipe(cleanCss())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css/'))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("css/"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
-gulp.task('watch', ['build-theme'], function() {
-  gulp.watch(['scss/*.scss'], ['build-theme']);
+gulp.task("watch", ["browserSync", "build-theme"], function() {
+  gulp.watch(["scss/*.scss"], ["build-theme"]);
 });
 
-gulp.task('default', ['build-theme'], function() {
-});
-
-
-
+gulp.task("default", ["build-theme"], function() {});
