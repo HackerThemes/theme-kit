cf3ea5f7ba95a3d9820020f59887d7bd01a183ae
Herwindo Artono (Windo) <herwindo.artono@go-jek.com>
2018-12-13 17:27:11 +0700

    add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,42 @@
-var gulp  = require('gulp'),
+var gulp = require('gulp'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   cleanCss = require('gulp-clean-css'),
   rename = require('gulp-rename'),
-  postcss      = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  livereload = require('gulp-livereload');
 
-gulp.task('build-theme', function() {
+gulp.task('build-theme', function () {
   return gulp.src(['scss/*.scss'])
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([ autoprefixer({ browsers: [
-      'Chrome >= 35',
-      'Firefox >= 38',
-      'Edge >= 12',
-      'Explorer >= 10',
-      'iOS >= 8',
-      'Safari >= 8',
-      'Android 2.3',
-      'Android >= 4',
-      'Opera >= 12']})]))
+    .pipe(postcss([autoprefixer({
+      browsers: [
+        'Chrome >= 35',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12']
+    })]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css/'))
     .pipe(cleanCss())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css/'))
+    .pipe(livereload())
 });
 
-gulp.task('watch', ['build-theme'], function() {
+gulp.task('watch', ['build-theme'], function () {
+  livereload.listen();
   gulp.watch(['scss/*.scss'], ['build-theme']);
 });
 
-gulp.task('default', ['build-theme'], function() {
+gulp.task('default', ['build-theme'], function () {
 });
 
 
