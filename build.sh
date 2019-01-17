6da20b5d521957d7543631a86cfe1a99442db540
Alexander Rechsteiner <alex@wildmind.ch>
2019-01-17 13:35:33 +0100

    Build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-rm dist/hackerthemes-theme-kit.zip
+rm dist/hackerthemes-theme-kit.zip -f
+mkdir dist -p
+gulp
 pushd ..
 find theme-kit \( -path '*/.*' -o -path 'theme-kit/node_modules*' -o -path 'dist*' \) -prune -o -type f -print | zip theme-kit/dist/hackerthemes-theme-kit.zip -@
 popd
