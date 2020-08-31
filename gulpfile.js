6ba966d988aa5c0be9c10cae5fd0d39ebf843d72
Carlos Álvarez <calvarezm70@gmail.com>
2020-08-31 17:06:32 -0600

    Update package versions

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,7 @@ function buildCss() {
     return gulp.src(['scss/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer({ browsers: [
-                'Chrome >= 35',
-                'Firefox >= 38',
-                'Edge >= 12',
-                'Explorer >= 10',
-                'iOS >= 8',
-                'Safari >= 8',
-                'Android 2.3',
-                'Android >= 4',
-                'Opera >= 12']})]))
+        .pipe(postcss([ autoprefixer()]))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css/'))
         .pipe(cleanCss())
